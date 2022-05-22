d4c88e61b288381bedaa0f24cc3c5dd1f7901d8a
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-05-22 23:53:34 +0200

    resuelto problema relaciones

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,7 +7,7 @@ create table peliculas(
 	calificacion_edad int
 	);
 create table salas (
-	codigo int PRIMARY KEY,
+	id int PRIMARY KEY,
 	nombre varchar(100),
 	pelicula_id int,
 	CONSTRAINT pelicula_fk FOREIGN KEY(pelicula_id) REFERENCES peliculas(codigo) ON DELETE CASCADE ON UPDATE CASCADE);
@@ -16,6 +16,6 @@ insert into peliculas (codigo, nombre, calificacion_edad) values (1, 'Baterman',
 insert into peliculas (codigo, nombre, calificacion_edad) values (2, 'El transpasable', 18);
 insert into peliculas (codigo, nombre, calificacion_edad) values (3, 'Sopaman', 16);
 
-insert into salas (codigo, nombre, pelicula_id) values (1, 'A1', 3);
-insert into salas (codigo, nombre, pelicula_id) values (2, 'A2', 1);
-insert into salas (codigo, nombre, pelicula_id) values (3, 'A3', 2);
+insert into salas (id, nombre, pelicula_id) values (1, 'A1', 3);
+insert into salas (id, nombre, pelicula_id) values (2, 'A2', 1);
+insert into salas (id, nombre, pelicula_id) values (3, 'A3', 2);
